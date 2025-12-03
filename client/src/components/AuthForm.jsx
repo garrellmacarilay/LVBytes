ef83c29582c14867af0887d3562665423726f982
GarrellMac <gmacarilay1@gmail.com>
2025-12-04 05:25:48 +0800

    Done with Lightning CSS install

--- client/src/components/AuthForm.jsx
+++ client/src/components/AuthForm.jsx
@@ -3,26 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { signupUser, loginUser } from "../services/userService";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../lib/firebase";
-
-// Shield Icon Component
-const Shield = (props) => (
-  <svg
-    {...props}
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={2}
-    stroke="currentColor"
-  >
-    <path
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
-    />
-  </svg>
-);
+import { Shield } from './Icons'
 
 export default function AuthForm() {
+
   const navigate = useNavigate();
   const [isLogin, setIsLogin] = useState(true);
   const [email, setEmail] = useState("");
